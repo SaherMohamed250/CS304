c2e77f26a8ee0f0cc698339b6e82cf01f68c80a2
Saif_Ashraf <112732757+SaifAshraf22@users.noreply.github.com>
2022-12-26 03:50:13 +0200

    Update DrawGraphic.java
    the way to show blocks in panel

--- cs304/src/DrawGraphic.java
+++ cs304/src/DrawGraphic.java
@@ -3,16 +3,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.Panel;
 
-public class DrawGraphic extends Panel {
+public class DrawGraphic extends JPanel implements KeyListener ,ActionListener{
     private int score = 0;
     private int hockx =200;
     private int ballx=200;
     private int bally=200;
+    private boolean play=false;
+     private Blocks mapPlay;
+
 
     public void paint(Graphics g){
         
         setBackground(Color.BLACK);
 
+        //create blocks 
+        mapPlay = new Blocks(5, 10);
         //Border
         g.setColor(Color.red);
         g.fillRect(0, 0, 3, 592);
@@ -22,7 +27,9 @@ public class DrawGraphic extends Panel {
         //hock
         g.setColor(Color.green);
         g.fillRect(hockx,550,100,8);
-
+        
+        //coloerd the blocks
+        mapPlay.draw((Graphics) graphics, Color.yellow);
        //ball
         g.setColor(Color.YELLOW);
         g.fillOval(ballx,bally,20,20);
@@ -31,5 +38,30 @@ public class DrawGraphic extends Panel {
          g.setColor(Color.white);
         g.setFont(new Font("script",Font.BOLD, 25));
         g.drawString(""+score, 590,30);
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (play) {
+ //to show the blocks 
+           
+            for (int i = 0; i < mapPlay.map.length; i++) {
+                for (int j = 0; j < mapPlay.map[i].length; j++) {
+                    if (mapPlay.map[i][j] > 0) {
+        }
+    }
+}
+        }
     }
 }
