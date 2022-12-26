cbeb7287f60f7c606f549c21bb78bb4c2b066b01
BassantNaser5 <121403738+BassantNaser5@users.noreply.github.com>
2022-12-26 14:57:43 +0200

    Update DrawGraphic.java

--- cs304/src/DrawGraphic.java
+++ cs304/src/DrawGraphic.java
@@ -1,47 +1,94 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class DrawGraphic extends Panel implements KeyListener ,ActionListener{
+public class DrawGraphic extends JPanel implements KeyListener, ActionListener {
     private int score = 0;
+    private int ballx,bally,x1,y1,bounds=30;
+    private boolean move_up,move_left,move_dawn,move_right;
+
     private int hockx =200;
-    private int ballx=200;
-    private int bally=200;
+
     private boolean play=false;
-     private Blocks mapPlay;
+    private Blocks mapPlay;
+    public DrawGraphic(int a, int b) {
+        Timer timer = new Timer(5, new ActionListener() {
+            //bouncing ball
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Math.random();
+                if (ballx > getWidth() - bounds-10) {
+                    move_left = true;
+                }
+                if (ballx < 0) {
+                    move_left = false;
+                }
+                if (move_left) {
+                    ballx -= a;
+                } else {
+                    ballx += a;
+                }
+                if (bally > getHeight() - bounds-10) {
+                    move_up = true;
+                }
+                if (bally < 0) {
+                    move_up = false;
+                }
+                if (move_up) {
+                    bally -= b;
+           }
+
+                else {
+                    bally += b;
+                }
+
+
+                repaint();
+            }
+
+
+        });
+        timer.start();
+    }
 
 
     public void paint(Graphics g){
-        
-        setBackground(Color.BLACK);
+            setBackground(Color.black);
+            Graphics2D g1= (Graphics2D) g;
+            Graphics2D g2 = (Graphics2D) g;
+            Graphics2D g3 = (Graphics2D) g;
+        Graphics2D g4 = (Graphics2D) g;
+        Graphics2D g5 = (Graphics2D) g;
 
-        //create blocks 
+            super.paint(g1);
+        //create blocks
         mapPlay = new Blocks(5, 10);
         //Border
-        g.setColor(Color.red);
-        g.fillRect(0, 0, 3, 592);
-        g.fillRect(0, 0, 692, 3);
-        g.fillRect(691, 0, 3, 592);
+        g2.setColor(Color.blue);
+        g2.fillRect(0, 0, 3, 592);
+        g2.fillRect(0, 0, 692, 3);
+        g2.fillRect(680, 0, 3, 592);
+
 
         //hock
-        g.setColor(Color.green);
-        g.fillRect(hockx,550,100,8);
-        
+        g3.setColor(Color.green);
+        g3.fillRect(hockx,550,100,8);
+
         //coloerd the blocks
         mapPlay.draw((Graphics) g, Color.white);
-       //ball
-        g.setColor(Color.YELLOW);
-        g.fillOval(ballx,bally,20,20);
-        
+
+        //ball
+        g1.setColor(Color.blue);
+        g1.fillOval(ballx,bally,bounds,bounds);
+
         //score
-         g.setColor(Color.white);
-        g.setFont(new Font("script",Font.BOLD, 25));
-        g.drawString(""+score, 590,30);
+        g4.setColor(Color.white);
+        g4.setFont(new Font("script",Font.BOLD, 25));
+        g4.drawString(""+score, 590,30);
     }
     @Override
     public void keyTyped(KeyEvent e) {
@@ -58,14 +105,14 @@ public class DrawGraphic extends Panel implements KeyListener ,ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if (play) {
- 
-   //to show the blocks   
+
+            //to show the blocks
             for (int i = 0; i < mapPlay.map.length; i++) {
                 for (int j = 0; j < mapPlay.map[i].length; j++) {
                     if (mapPlay.map[i][j] > 0) {
-        }
-    }
-}
+                    }
+                }
+            }
         }
     }
 }
