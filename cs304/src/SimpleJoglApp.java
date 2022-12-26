cc403d732890b0ae17cb4b608d394a42d8c3acec
BassantNaser5 <121403738+BassantNaser5@users.noreply.github.com>
2022-12-26 14:58:40 +0200

    Update SimpleJoglApp.java

--- cs304/src/SimpleJoglApp.java
+++ cs304/src/SimpleJoglApp.java
@@ -1,21 +1,15 @@
 
 
-import com.sun.opengl.util.FPSAnimator;
-
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
-import javax.media.opengl.*;
-import javax.swing.Timer;
 
 
 public class SimpleJoglApp {
 
     public static void main(String[] args) {
-         JFrame j = new JFrame();
-        DrawGraphic draw =new DrawGraphic();
+        JFrame j = new JFrame();
+        DrawGraphic draw = new DrawGraphic(5,5);
 
-        j.setBounds(10,10,700,600);
+        j.setBounds(10, 10, 700, 600);
         j.setTitle("BOUNCE BALL");
         j.setResizable(false);
         j.setVisible(true);
@@ -23,5 +17,5 @@ public class SimpleJoglApp {
         j.add(draw);
         j.setVisible(true);
     }
+
 }
-   
