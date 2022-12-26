025250a4345f5f1c553d95de567f75bf6954817e
saher mohamed <120128502+SaherMohamed250@users.noreply.github.com>
2022-12-26 17:55:56 +0200

    Update DrawGraphic.java

--- cs304/src/DrawGraphic.java
+++ cs304/src/DrawGraphic.java
@@ -1,10 +1,7 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 
 public class DrawGraphic extends JPanel implements KeyListener, ActionListener , MouseMotionListener{
     private int score = 0;
