e7e6078c85be32f3f16aae052b6cb6f753c8fed0
Youssef Ashraf <120688787+YoussefAshraf22@users.noreply.github.com>
2022-12-27 21:10:08 +0200

    My last update

--- cs304/src/gameScreen.java
+++ cs304/src/gameScreen.java
@@ -1,23 +1,32 @@
-
 package bounceballgame;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.Timer;
+import javax.swing.JFrame;
 
-// you must write the drow graphics class to run the gameplay
+public class gameScreen {
 
-public class gameScreen extends JFrame {
-    JLabel label = new JLabel("Hello Bounce Ball");
     public gameScreen(){
-        super("Game Screen");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         this.setSize(700,600);
-         this.setLocationRelativeTo(null);
-         this.setVisible(true);
-             
-         label.setBounds(100,100,100,50);
-         label.setFont(new Font(null,Font.PLAIN,25));
-         this.add(label);
+        JFrame j =new JFrame();
+        thegameH game= new thegameH();
+        j.setBounds(10,10,700,600);
+        j.setTitle("Bounce Ball");
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.add(game);
+        j.addKeyListener(game);
+        j.addMouseMotionListener(game);
+        j.setResizable(false);
+        j.setLocationRelativeTo(null);
+        j.setVisible(true);
     }
 }
