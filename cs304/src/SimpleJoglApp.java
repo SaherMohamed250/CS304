153b9a7caeeda6494f23aa0dd0cfb3fa920bedb7
Eman Ehab <121316573+emanehab12@users.noreply.github.com>
2022-12-27 21:41:34 +0200

    class last update

--- cs304/src/SimpleJoglApp.java
+++ cs304/src/SimpleJoglApp.java
@@ -14,18 +14,7 @@ import javax.swing.Timer;
 public class SimpleJoglApp throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
     public static void main(String[] args) {
-        JFrame j = new JFrame();
-        DrawGraphic draw = new DrawGraphic(5,5);
-
-        j.setBounds(10, 10, 700, 600);
-        j.setTitle("BOUNCE BALL");
-        j.setResizable(false);
-        j.setVisible(true);
-        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        j.add(draw);
-        j.addKeyListener(draw);
-        j.addMouseMotionListener(draw);
-        j.setVisible(true);
+        new Menu();     
         Scanner scanner = new Scanner(System.in);
         File file = new File("file_example_WAV_2MG.wav");
         AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
@@ -34,6 +23,19 @@ public class SimpleJoglApp throws UnsupportedAudioFileException, IOException, Li
         clip.start();
         clip.loop(10000);
         String response = scanner.next();
-    }
+   }
 
+    
+    public theGame(){
+        JFrame j =new JFrame();
+        Menu M = new Menu();
+   
+        j.setBounds(10,10,700,600);
+         j.setTitle("Bounce Ball");
+        j.setVisible(true);
+        j.setLayout(null);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         j.setResizable(false);
+        j.setLocationRelativeTo(null);
+    }
 }
